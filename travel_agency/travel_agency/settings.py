e3b60cd049c82e3bc7998e4507464eaf8aeefbf3
felixbadea <badea_felix@yahoo.com>
2025-04-20 12:10:53 +0300

    verificat settings.py

--- travel_agency/travel_agency/settings.py
+++ travel_agency/travel_agency/settings.py
@@ -127,7 +127,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'trips' / 'static',
 ]
-
 # Pentru producție (unde se adună fișierele cu collectstatic)
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
